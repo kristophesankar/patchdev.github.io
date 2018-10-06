93a335274405eb9bd4a6f37962bb214560e44e78
kristophesankar <kristophe.lukas.sankar@gmail.com>
2018-10-06 18:01:29 -0400

    Fixed sidebar navigation mobile issue

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,15 +1,15 @@
 <template>
 <div id="main" class="main">
-  <div class="row">
+  <div class="row ">
     <div class="main-headings col-md-12">
-          <a href="javascript:void(0)" class="custom-nav-drawer " v-on:click="toggleNav">
-            <font-awesome-icon icon="bars" class="pull-right"  />
-          </a>
-        </div>
+      <a href="javascript:void(0)" class="custom-nav-drawer " v-on:click="toggleNav">
+        <font-awesome-icon icon="bars"   />
+      </a>
+    </div>
   </div>
   <section id="top" class="top-section">
-		<div class="row align-items-center">
-      <div class="col-md-12 mx-auto">
+		<div class="row align-items-center typed-row">
+      <div class="col-md-10 mx-auto">
         <div class="h-100 border-primary justify-content-center text-right "> 
           <vue-typed-js :strings="[`Agile^1000`,`Data Driven^1000`, `Pattern Oriented^1000`, `Test Driven^1000`,`Performant^1000`, ]" :typeSpeed="90" :backSpeed="90" >
             <h1 >
